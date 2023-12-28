9a6e1f1e347a72fb256c9b34aa5db2edbbecafe6
LiWen <3552214025@qq.com>
2023-12-28 22:19:51 +0800

    Site updated: 2023-12-28 22:19:50

--- HTML/2/file/functions.js
+++ HTML/2/file/functions.js
@@ -38,23 +38,34 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var seconds = 1000;
+	var minutes = seconds * 60;
+	var hours = minutes * 60;
+	var days = hours * 24;
+	var years = days * 365;
+	var today = new Date();
+	var startYear = 2023;
+	var startMonth = 5;
+	var startDate = 25;
+	var startHour = 18;
+	var startMinute = 7;
+	var startSecond = 0;
+	var todayYear = today.getFullYear();
+	var todayMonth = today.getMonth() + 1;
+	var todayDate = today.getDate();
+	var todayHour = today.getHours();
+	var todayMinute = today.getMinutes();
+	var todaySecond = today.getSeconds();
+	var t1 = Date.UTC(startYear, startMonth, startDate, startHour, startMinute, startSecond);
+	var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
+	var diff = t2 - t1;
+	var diffYears = Math.floor(diff / years);
+	var diffDays = Math.floor((diff / days) - diffYears * 365);
+	var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
+	var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) /
+	  minutes);
+	var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours -
+	  diffMinutes * minutes) / seconds);
+	var result = "第 <span class=\"digit\">" + diffDays + "</span> 天 <span class=\"digit\">" + diffHours + "</span> 小时 <span class=\"digit\">" + diffMinutes + "</span> 分钟 <span class=\"digit\">" + diffSeconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
